16f039e17e62b103ac31eafcbb0054594148dbe9
akletus <ak@letus.kitchen>
2025-03-03 17:34:41 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,24 +4,24 @@ import { readdirSync, readFileSync, statSync } from "node:fs"
 
 const router = new Router()
 
-router.post('/search', async ({req, res}, next) => {
-    const c = req.query.category as 'books' | 'movies' | 'music'
-    const q = req.query.term as string
+router.post('/search', async ({request, response}) => {
+    const c = request.query.category as 'books' | 'movies' | 'musics'
+    const q = request.query.term as string
 
     try {
         const files = searchFilesInCategory(c, q)
-        res.body = JSON.stringify({ files })
-        res.type = 'json'
-    } catch (e) {
-        res.body = JSON.stringify({ error: e.message })
-        res.type = 'json'
+        response.body = JSON.stringify({ files })
+        response.type = 'json'
+    } catch (error) {
+        response.body = JSON.stringify({ error })
+        response.type = 'json'
     }
 })
 
-function searchFilesInCategory(c: 'books' | 'movies' | 'music', q: string): string[] {
+function searchFilesInCategory(c: 'books' | 'movies' | 'musics', q: string): string[] {
     return readdirSync(`./data/${c}`)
-        .filter(x => statSync(`./data/${c}/${x}`).isFile())
-        .filter(x => readFileSync(`./data/${c}/${x}`, 'utf-8').includes(q))
+        .filter((x: string) => statSync(`./data/${c}/${x}`).isFile())
+        .filter((x: string) => readFileSync(`./data/${c}/${x}`, 'utf-8').toLowerCase().includes(q.toLowerCase()))
 }
 
-new Koa().use(router.routes()).listen(80, () => {})
+new Koa().use(router.routes()).listen(8080, () => {})
